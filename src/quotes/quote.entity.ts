98fd8050a6a23a4b31efae394eb5123fa3ad7587
dropRole <dusan.radosavljevic82@gmail.com>
2022-12-22 19:39:52 +0100

    Database implementation and TypeORM integration

---
+++ src/quotes/quote.entity.ts
@@ -0,0 +1,20 @@
+import { User } from 'src/auth/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+
+@Entity('quotes')
+export class Quote {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'character varying', length: 400 })
+  quote: string;
+
+  @Column({ type: 'smallint' })
+  upvotes: number;
+
+  @Column({ type: 'smallint' })
+  downvotes: number;
+
+  @ManyToOne((_type) => User, (user) => user.quotes, { eager: false })
+  user: User;
+}
