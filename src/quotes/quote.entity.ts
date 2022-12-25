f3c68ecc35cbac1c808f2d7f3c6ade0a563b34c5
dropRole <dusan.radosavljevic82@gmail.com>
2022-12-25 12:57:38 +0100

    Script relative path fix

--- src/quotes/quote.entity.ts
+++ src/quotes/quote.entity.ts
@@ -1,4 +1,4 @@
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 
 @Entity('quotes')
