7163e6d7daa83a6204ea3fed21b3ac0559dd7d96
droprole <dusan.radosavljevic82@gmail.com>
2026-04-09 22:38:26 +0200

    Modified TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,29 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { QuotesModule } from './quotes/quotes.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { EnvConfig } from './config/env.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeORMAsyncConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     AuthModule,
     QuotesModule,
     ConfigModule.forRoot(EnvConfig),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('PG_HOST'),
-        port: configService.get('PG_PORT'),
-        database: configService.get('PG_DB'),
-        username: configService.get('PG_USER'),
-        password: configService.get('PG_PASS'),
-        autoLoadEntities: true,
-        synchronize: process.env.STAGE === 'dev' ? true : false,
-      }),
-    }),
+    TypeOrmModule.forRootAsync(TypeORMAsyncConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
