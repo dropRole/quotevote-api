8b68be949324bd207efa5e022528c028e3bf87ba
droprole <dusan.radosavljevic82@gmail.com>
2026-04-12 19:53:06 +0200

    Registered entity repos, corrected eagerness in user-quote relation

--- src/quotes/quotes.module.ts
+++ src/quotes/quotes.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { QuotesController } from './quotes.controller';
 import { QuotesService } from './quotes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Quote } from './entities/quote.entity';
+import { Vote } from './entities/vote.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Quote, Vote])],
   controllers: [QuotesController],
-  providers: [QuotesService]
+  providers: [QuotesService],
 })
 export class QuotesModule {}
