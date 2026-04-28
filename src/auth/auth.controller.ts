953031b73264a4e4c1fd6ee4a950b453dfe2cd1b
droprole <dusan.radosavljevic82@gmail.com>
2026-04-28 17:13:12 +0200

    Enabled cookie mechanism

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,6 +15,7 @@ import {
   Header,
   Query,
   Delete,
+  Res,
 } from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { createReadStream } from 'fs';
@@ -27,6 +28,7 @@ import { SignUpDTO } from './dto/sign-up.dto';
 import { GetUser } from '../common/decorators/get-user.decorator';
 import { Public } from '../common/decorators/public.decorator';
 import { User } from './entities/user.entity';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -42,9 +44,10 @@ export class AuthController {
   @Public()
   @Post('/login')
   login(
+    @Res({ passthrough: true }) response: Response,
     @Body() authCredentialsDTO: AuthCredentialsDTO,
-  ): Promise<{ accessToken: string }> {
-    return this.authService.login(authCredentialsDTO);
+  ): Promise<void> {
+    return;
   }
 
   @Get('/me')
@@ -63,10 +66,11 @@ export class AuthController {
 
   @Patch('/me/basics')
   updateBasics(
+    @Res({ passthrough: true }) response: Response,
     @GetUser() user: User,
     @Body() basicsUpdateDTO: BasicsUpdateDTO,
-  ): Promise<{ accessToken: string }> {
-    return this.authService.updateBasics(user, basicsUpdateDTO);
+  ): Promise<void> {
+    return;
   }
 
   @Patch('/me/pass')
