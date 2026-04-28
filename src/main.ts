02f44d991c9efce3c5f3bbc7a7aaf8aa4cb1187d
droprole <dusan.radosavljevic82@gmail.com>
2026-04-28 17:13:12 +0200

    Enabled cookie mechanism

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  app.enableCors();
+  app.enableCors({ origin: process.env.QUOTEVOTE_SPA_URL, credentials: true });
+  app.use(cookieParser());
   await app.listen(process.env.PORT);
 }
 bootstrap();
