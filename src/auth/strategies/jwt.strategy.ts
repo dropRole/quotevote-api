f64690cf77c283aa90f6ee613bddc09acffb8af8
droprole <dusan.radosavljevic82@gmail.com>
2026-05-01 14:06:19 +0200

    Extracted JWT from cookie

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,13 +3,16 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JWTPayload } from './jwt-payload.interface';
+import { Request } from 'express';
 
 @Injectable()
 export class JWTStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
       secretOrKey: configService.get('JWT_SECRET'),
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request.cookies['quotevote-jwt'],
+      ]),
     });
   }
 
